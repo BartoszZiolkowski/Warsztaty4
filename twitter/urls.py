d8b0f49b1e558917bbda71da84970df271504aa3
Bartosz Ziółkowski <bartosz.k.ziolkowski@gmail.com>
2018-08-18 14:31:18 +0200

    zad1

--- twitter/urls.py
+++ twitter/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
+from twitterapp.views import BaseView, Login, Logout
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',BaseView.as_view(), name="base_view"),
+    url(r'^login', Login.as_view(), name='login-form'),
+    url(r'^logout', Logout.as_view(), name='logout'),
+
+
 ]
--- twitterapp/views.py
+++ twitterapp/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
 
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic import DeleteView, CreateView
+
+#from .models import
+#from .forms import
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
+from django.views import View
+
+
+class BaseView(View):
+    def get(self, request):
+        base = []
+        return render(request, "base.html", {'base':base})
+
+    def post(self, request):
+        return HttpResponse('post')
+
+
+class Login(View):
+    def get(self, request):
+        return HttpResponse
+    def post(self, request):
+        return HttpResponse
+
+class Logout(View):
+    def get(self, request):
+        return HttpResponse
+    def post(self, request):
+        return HttpResponse
